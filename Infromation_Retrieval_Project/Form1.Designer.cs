52805df1bc9b40999a5b3ed5c735153b2c731da7
AhmedHassaan <ahmedhassaan162@gmail.com>
2019-03-13 17:23:00 +0200

    Start code

---
+++ Infromation_Retrieval_Project/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace Infromation_Retrieval_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.stopBtn = new System.Windows.Forms.Button();
+            this.allSize = new System.Windows.Forms.Label();
+            this.visitedSize = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(19, 12);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(109, 46);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(19, 169);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(383, 251);
+            this.listBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.label1.Location = new System.Drawing.Point(360, 72);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.label2.Location = new System.Drawing.Point(117, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "All";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.label3.Location = new System.Drawing.Point(477, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Visisted";
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(408, 169);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(383, 251);
+            this.listBox2.TabIndex = 4;
+            // 
+            // stopBtn
+            // 
+            this.stopBtn.Location = new System.Drawing.Point(19, 72);
+            this.stopBtn.Name = "stopBtn";
+            this.stopBtn.Size = new System.Drawing.Size(109, 46);
+            this.stopBtn.TabIndex = 7;
+            this.stopBtn.Text = "Stop";
+            this.stopBtn.UseVisualStyleBackColor = true;
+            this.stopBtn.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // allSize
+            // 
+            this.allSize.AutoSize = true;
+            this.allSize.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.allSize.Location = new System.Drawing.Point(155, 128);
+            this.allSize.Name = "allSize";
+            this.allSize.Size = new System.Drawing.Size(32, 24);
+            this.allSize.TabIndex = 8;
+            this.allSize.Text = "All";
+            // 
+            // visitedSize
+            // 
+            this.visitedSize.AutoSize = true;
+            this.visitedSize.Font = new System.Drawing.Font("Tahoma", 15F);
+            this.visitedSize.Location = new System.Drawing.Point(562, 128);
+            this.visitedSize.Name = "visitedSize";
+            this.visitedSize.Size = new System.Drawing.Size(79, 24);
+            this.visitedSize.TabIndex = 9;
+            this.visitedSize.Text = "Visisted";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(794, 462);
+            this.Controls.Add(this.visitedSize);
+            this.Controls.Add(this.allSize);
+            this.Controls.Add(this.stopBtn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.startBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button stopBtn;
+        private System.Windows.Forms.Label allSize;
+        private System.Windows.Forms.Label visitedSize;
+    }
+}
+
---
+++ Infromation_Retrieval_Project/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using mshtml;
+using System.Threading;
+
+namespace Infromation_Retrieval_Project
+{
+    public partial class Form1 : Form
+    {
+        List<string> allLinks;
+        List<string> visitedLinks;
+        List<string> unvisitedLinks;
+        WebRequest myWebRequest;
+        WebResponse myWebResponse;
+        Stream streamResponse;
+        StreamReader sReader;
+        private System.Windows.Forms.Timer timer1;
+        int seconds = 0;
+        int minute = 0;
+        int hours = 0;
+        Thread mainThread;
+        public Form1()
+        {
+            InitializeComponent();
+            stopBtn.Visible = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            stopBtn.Visible = true;
+            allLinks = new List<string>();
+            visitedLinks = new List<string>();
+            unvisitedLinks = new List<string>();
+            string urll = "http://www.egypttoday.com";
+            //allLinks.Add(urll);
+            //unvisitedLinks.Add(urll);
+            //urll = "http://www.cnn.com";
+            allLinks.Add(urll);
+            unvisitedLinks.Add(urll);
+            mainThread = new Thread(threadF);
+            mainThread.Start();
+            timer1 = new System.Windows.Forms.Timer();
+            timer1.Tick += new EventHandler(timer1_Tick);
+            timer1.Interval = 1000; // 1 second
+            timer1.Start();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            seconds++;
+            if (seconds == 60)
+            {
+                minute++;
+                seconds = 0;
+            }
+            if (minute == 60)
+            {
+                hours++;
+                minute = 0;
+            }
+            label1.Text = hours + ":" + minute + ":" + seconds;
+        }
+
+
+        void threadF()
+        {
+
+            int count = 0;
+            while (unvisitedLinks.Count > 0)
+            {
+                count++;
+                String URL = unvisitedLinks[0];
+                unvisitedLinks.RemoveAt(0);
+                try
+                {
+                    // Create a new 'WebRequest' object to the mentioned URL.
+                    myWebRequest = WebRequest.Create(URL);
+                    // The response object of 'WebRequest' is assigned to a WebResponse' variable.
+                    myWebResponse = myWebRequest.GetResponse();
+                    streamResponse = myWebResponse.GetResponseStream();
+                    sReader = new StreamReader(streamResponse);
+                    string rString = sReader.ReadToEnd();
+                    HTMLDocument y = new HTMLDocument();
+                    IHTMLDocument2 doc = (IHTMLDocument2)y;
+                    doc.write(rString);
+                    //MessageBox.Show(doc.links.toString());
+                    IHTMLElementCollection elements = doc.links;
+                    foreach (IHTMLElement el in elements)
+                    {
+                        string temp = (string)el.getAttribute("href", 0);
+                        if (temp.StartsWith("http") || temp.StartsWith("/"))
+                        {
+                            if (temp.StartsWith("/"))
+                                temp = URL + temp;
+                            if (!allLinks.Contains(temp))
+                            {
+                                allLinks.Add(temp);
+                                unvisitedLinks.Add(temp);
+                            }
+                        }
+
+                    }
+                    visitedLinks.Add(URL);
+                }
+                catch 
+                {
+                    count--;
+                }
+                visitedSize.Text = visitedLinks.Count.ToString();
+                allSize.Text = allLinks.Count.ToString();
+                listBox2.DataSource = null;
+                listBox2.DataSource = visitedLinks;
+                listBox1.DataSource = null;
+                listBox1.DataSource = allLinks;
+                //if (count == 20)
+                //    break;
+            }
+            streamResponse.Close();
+            sReader.Close();
+            myWebResponse.Close();
+            MessageBox.Show("Done");
+            timer1.Stop();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            mainThread.Abort();
+            streamResponse.Close();
+            sReader.Close();
+            myWebResponse.Close();
+            timer1.Stop();
+            MessageBox.Show("Done");
+        }
+    }
+}
